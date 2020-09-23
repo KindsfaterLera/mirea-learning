fdc9e7869ab9832efa0dbdc6ddde69703b32be4c
Lera <lera.kindsfater@gmail.com>
2020-09-23 20:37:23 +0300

    Практическая №3 от 17.09.2020 Задание №2

---
+++ 03.2.Dog-prog.java
@@ -0,0 +1,31 @@
+public abstract class Dog{
+
+    private String name;
+    private String breed;
+    private String color;
+    private int age;
+
+    public Dog(String name, int age){
+        this.name = name;
+        this.breed = breed;
+        this.color = color;
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getName(){
+        return breed;
+    }
+    public String getName(){
+        return color;
+    }
+    public int setAge(){
+        return age*7;
+    }
+
+    public String ToString(){
+        System.out.println("Name is " + name + ", dog breed is " + breed +  ", dog color is " + color + ", dog age is " + age);
+    }
+}
