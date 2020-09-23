304130af6753aa585fd3074502ebb4fd4d778d90
Lera <lera.kindsfater@gmail.com>
2020-09-23 20:42:06 +0300

    Практическая №3 от 17.09.2020 Задание №2 - Подкласс Овчарка

---
+++ 03.2.Shepherd-prog.java
@@ -0,0 +1,10 @@
+public class shepherd extends Dog {
+    public static void main(String args[]){
+        shepherd atos = new shepherd();
+        atos.setName("Atos");
+        atos.breed("ovcharka");
+        atos.color(blach & brown);
+        atos.setAge(3);
+        System.out.println(atos.toString());
+    }
+}
